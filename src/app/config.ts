b3c90f949998cc6bdac9af7f1ba9dbea533dd19c
Ruth Hdz <ruth.herlin@gmail.com>
2026-03-27 12:22:24 +0100

    added launches information and refactor structurero

---
+++ src/app/config.ts
@@ -0,0 +1,10 @@
+/**
+ * Global Application Configuration
+ */
+export const APP_CONFIG = {
+    version: '0.0.1',
+    environment: 'production',
+    serviceName: 'spacex-launchpad-explorer',
+    copyrightYear: 2026,
+  };
+  
