979e8107afeb9fa4ee12cc27627a88ae0e3af041
Ruth Hdz <ruth.herlin@gmail.com>
2026-03-27 18:41:12 +0100

    added tests, moved grid registry to main, added copilot instructions

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import {bootstrapApplication} from '@angular/platform-browser';
 import {App} from './app/app';
 import {appConfig} from './app/app.config';
+import { SetFilterModule } from 'ag-grid-enterprise';
+import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
 
+ModuleRegistry.registerModules([ AllCommunityModule, SetFilterModule]);
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
