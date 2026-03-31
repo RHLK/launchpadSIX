7cfe17089622f5b1d31d8297d28bf4a16d69d5a2
Ruth Hdz <ruth.herlin@gmail.com>
2026-03-31 16:51:58 +0200

    updated version

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,7 +2,7 @@
  * Global Application Configuration
  */
 export const APP_CONFIG = {
-  version: '1.0.0',
+  version: '1.0.1',
   environment: 'production',
   serviceName: 'spacex-launchpad-explorer',
   copyrightYear: 2026,
